94b98dd5cb9721e1d2d8bd1407ca1a05063080d2
Christian Ceciliano <cristiancehi@gmail.com>
2015-08-12 19:48:18 -0500

    + More Openshift Config

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ var app = require('./index');
 var http = require('http');
 
 var server;
+var serverPort = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+var serverIP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 /*
  * Create and start HTTP server.
  */
 
 server = http.createServer(app);
-server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8000);
+server.listen(serverPort, serverIP);
 server.on('listening', function() {
   console.log('Server listening on http://localhost:%d', this.address().port);
 });
