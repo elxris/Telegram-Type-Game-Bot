a8cb3cb77a7b5b83517dbdb9d444fcfaa020aec0
Christian Ceciliano <cristiancehi@gmail.com>
2015-08-12 22:14:28 -0500

    + First real example :)

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,25 @@
 'use strict';
+//jscs:disable requireCamelCaseOrUpperCaseIdentifiers
 
 var IndexModel = require('../models/index');
+var Telegram = require('../lib/telegram');
+var api = new Telegram(); // Preconfigured lib
 
 module.exports = function(router) {
   var model = new IndexModel();
 
   router.get('/', function(req, res) {
-    res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
+    res.send('ok');
+  });
+
+  router.get('/:token', function(req, res, next) {
+    if (req.params.token !== req.app.kraken.get('TELEGRAM_TOKEN')) {
+      res.status(400).send('Bad Token');
+    }
+    api.request('sendMessage', {
+      'chat_id': req.body.message.chat.id,
+      'text': 'Holo :)',
+      'reply_to_message_id': req.body.message.message_id
+    });
   });
 };
