d19132ffbf08e579b1133b8f1aec9a0d9f6d3a84
Christian Ceciliano <cristiancehi@gmail.com>
2015-08-12 19:26:37 -0500

    + Openshift Ready

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var server;
  */
 
 server = http.createServer(app);
-server.listen(process.env.PORT || 8000);
+server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8000);
 server.on('listening', function() {
   console.log('Server listening on http://localhost:%d', this.address().port);
 });
