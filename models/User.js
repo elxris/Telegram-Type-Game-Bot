74eb51beebde8e67152dd1b8b269666cde09fb70
Christian Ceciliano <cristiancehi@gmail.com>
2015-08-14 11:51:08 -0500

    + Preparando un nuevo feature, upgrades

--- models/User.js
+++ models/User.js
@@ -1,34 +1,61 @@
 'use strict';
 //jscs:disable requireCamelCaseOrUpperCaseIdentifiers
 
-var mongoose = require('mongoose');
-var Telegram = require('../lib/telegram');
-var api = new Telegram();
+var mongoose  = require('mongoose');
+var Telegram  = require('../lib/telegram');
+var Clicks    = require('../lib/clicks');
+var _         = require('lodash');
+var api       = new Telegram();
+
+var sendMessageError = function(id, message) {
+  api.request('sendMessage', {
+    chat_id: id,
+    text: message || '(501) Ocurrió un error. Inténtalo mas tarde.'
+  });
+};
 
 var userSchema = mongoose.Schema({
-  userid: Number,
+  userid: {
+    type: Number,
+    required: true
+  },
   requests: {
     type: Number,
     default: 0
+  },
+  lastRequest: {
+    type: Number,
+    default: Date.now
   },
   clicks: {
     type: Number,
     default: 0,
     index: true
-  }
+  },
+  lastClick: {
+    type: Number,
+    default: Date.now
+  },
+  upgrades: [{
+    id: {
+      type: String,
+      required: true
+    },
+    buyed: [{
+      type: Number
+    }]
+  }]
 });
 
-var sendMessageError = function(id) {
-  api.request('sendMessage', {
-    chat_id: id,
-    text: '(501) Ocurrió un error. Inténtalo mas tarde.'
-  });
-};
+userSchema.methods.getTotalClicks = Clicks();
 
 userSchema.statics.findTelegramUser = function(req, res, next) {
   var User = mongoose.model('User');
   var user = req.body.message.chat.id;
-  User.findOneAndUpdate({userid: user}, {$inc: {requests: 1}}, {upsert: true},
+  User.findOneAndUpdate(
+    {userid: user},
+    {$inc: {requests: 1}, $set: {lastRequest: Date.now()}},
+    {upsert: true},
     function(err, doc) {
       if (err) {
         sendMessageError(req.body.message.chat.id);
@@ -42,7 +69,43 @@ userSchema.statics.findTelegramUser = function(req, res, next) {
 
 userSchema.statics.incrementClicks = function(user, cb) {
   var User = mongoose.model('User');
-  User.findOneAndUpdate({_id: user.id}, {$inc: {clicks: 1}}, {new: true},
+  User.findOneAndUpdate(
+    {_id: user.id},
+    {$inc: {clicks: 1}, $set: {lastClick: Date.now()}},
+    {new: true},
+    function(err, doc) {
+      if (err) {
+        sendMessageError(user.userid);
+        return console.error(err);
+      }
+      cb(doc);
+    }
+  );
+};
+
+userSchema.statics.buyUpgrade = function(user, upgrade, cb) {
+  var User = mongoose.model('User');
+  if (user.getTotalClicks() < upgrade.getCost(user)) {
+    return sendMessageError(user.userid,
+      'No tienes suficientes clicks para comprarlo.');
+  }
+
+  var findQuery = {
+    _id: user.id, upgrades: {id: upgrade.id}
+  };
+  var updateQuery = {
+    $push: {'upgrades.$.buyed': Date.now()},
+    $set: {lastBuy: Date.now()}
+  };
+
+  // First buy
+  if (!_.filter(user.upgrades, _.matches({id: upgrade.id}))) {
+    delete findQuery.upgrades;
+    delete updateQuery.$push;
+    updateQuery.$addToSet = {upgrades: {id: upgrade.id, buyed: [Date.now()]}};
+  }
+
+  User.findOneAndUpdate(findQuery, updateQuery, {new: true},
     function(err, doc) {
       if (err) {
         sendMessageError(user.userid);
@@ -55,6 +118,7 @@ userSchema.statics.incrementClicks = function(user, cb) {
 
 userSchema.statics.resetUser = function(user, cb) {
   var User = mongoose.model('User');
+  // Todo, add score before reset.
   User.findOneAndUpdate({_id: user.id}, {$set: {clicks: 0}}, {new: true},
     function(err, doc) {
       if (err) {
