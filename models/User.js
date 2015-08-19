780d72f00ac1168f9fef53f33d5c690b9f2030ea
Christian Ceciliano <cristiancehi@gmail.com>
2015-08-19 01:52:43 -0500

    + Refactoring code

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 'use strict';
 //jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+//jshint maxcomplexity:10
 
 // Requires
 var mongoose          = require('mongoose');
@@ -14,7 +15,7 @@ var _                 = require('lodash');
 var INTENTS           = 10;
 
 // User schema
-var userSchema = mongoose.Schema({
+var userSchema = new mongoose.Schema({
   userid: {
     type: Number,
     required: true,
@@ -48,13 +49,14 @@ var userSchema = mongoose.Schema({
   }]
 });
 
-userSchema.methods.getTotalClicks = Clicks.getTotalClicks;
-
-/*
-++ express middleware
-**  Busca un usuario en la base de datos, o inserta un registro nuevo,
-**  y lo asigna a req.user
-*/
+/**
+ * Busca un usuario en la base de datos, o inserta un registro nuevo,
+ * y lo asigna a req.user
+ *
+ * @param {request} req - Express Request Object
+ * @param {response} res - Express Response Object
+ * @param {next} next - Express Next Callback
+ */
 userSchema.statics.findTelegramUser = function(req, res, next) {
   var User = mongoose.model('User');
   var user = req.body.message.chat.id;
@@ -69,38 +71,71 @@ userSchema.statics.findTelegramUser = function(req, res, next) {
   );
 };
 
-userSchema.statics.incrementClicks = function incrementClicks(user, cb, count) {
-  if (!count) {
-    count = 0;
+/**
+ * Realiza una operación de actualización de un usuario a la DB, pero
+ * cubriendo el requerimento de que el query de la búsqueda sea específico
+ * para que la operación de actualización no sea sobre un usuario ya modificado,
+ * por lo que automáticamente se añade al query.requests === user.requests.
+ *
+ * @param {Object} opts - Objeto de opciones
+ * @param {Object} opts.user - User document
+ * @param {(Object|Function)} [opts.query] - Mongo find query
+ * @param {(Object|Function)} opts.update - Mongo update query
+ * @param {Object} [opts.limit=10] - Límite de reintentos
+ * @param {Object} [opts.count=0] - Control interno del contador
+ * @param {callback} cb - Cuando la operación termina satisfactoria o hay
+ *  un error, llama al este callback.
+ */
+userSchema.statics.intent = function intent(opts, cb) {
+  opts = opts || {};
+  var user = opts.user;
+  if (!user) {
+    return cb(new Error('Undefined User'));
   }
+
+  opts = _.defaultsDeep(opts, {
+    count: 0,
+    query: {
+      userid: user.userid,
+      requests: user.requests
+    },
+    limit: INTENTS
+  });
+
+  var query = opts.query;
+  if (typeof opts.query === 'function') {query = opts.query(user);}
+  if (query instanceof Error) {return cb(query);}
+
+  var update = opts.update;
+  if (typeof opts.update === 'function') {query = opts.update(user);}
+  if (update instanceof Error) {return cb(update);}
+
   var User = mongoose.model('User');
   User.findOneAndUpdate(
-    {userid: user.userid, requests: user.requests},
-    {
-      $inc: {clicks: 1},
-      $set: {lastClick: Date.now()}
-    },
+    query || {},
+    update || {},
     {new: true},
     function(err, doc) {
       if (err) {console.error(err); return cb(err, doc);}
 
       if (!doc) {
-        if (count >= INTENTS) {
+        if (opts.count >= opts.limit) {
           err = new (Errors.DBUpdateError)('Demasiados intentos.');
           console.error(err);
           return cb(err, doc);
         }
 
+        delete query.requests;
         setTimeout(function() {
           User.findOne(
-            {userid: user.userid},
+            query,
             function(err, doc) {
               if (err) {console.error(err); return cb(err, doc);}
-
-              return incrementClicks(doc, cb, ++count);
+              opts.user = doc;
+              return intent(opts, cb);
             }
           );
-        }, 250 * count + Math.random() * 250);
+        }, 250 * opts.count + Math.random() * 250);
         return;
       }
       cb(err, doc);
@@ -108,56 +143,49 @@ userSchema.statics.incrementClicks = function incrementClicks(user, cb, count) {
   );
 };
 
-userSchema.statics.buyUpgrade = function buyUpgrade(user, upgrade, cb, count) {
-  if (!count) {
-    count = 0;
-  }
+userSchema.methods.getTotalClicks = Clicks.getTotalClicks;
 
+userSchema.methods.incrementClicks = function(cb) {
+  var user = this;
   var User = mongoose.model('User');
-  if (user.getTotalClicks() < upgrade.getCost(user)) {
-    cb(new UserStatusError('No tienes suficientes clicks para vender.'), user);
-  }
-
-  var findQuery = {
-    userid: user.userid,
-    requests: user.requests,
-    upgrades: {id: upgrade.id}
-  };
-  var updateQuery = {
-    $push: {'upgrades.$.buyed': Date.now()},
-    $set: {lastBuy: Date.now()}
-  };
-
-  // First buy
-  if (!_.filter(user.upgrades, _.matches({id: upgrade.id}))) {
-    delete findQuery.upgrades;
-    delete updateQuery.$push;
-    updateQuery.$addToSet = {upgrades: {id: upgrade.id, buyed: [Date.now()]}};
-  }
-
-  User.findOneAndUpdate(findQuery, updateQuery, {new: true},
-    function(err, doc) {
-      if (err) {console.error(err); return cb(err, doc);}
-
-      if (!doc) {
-        if (count >= INTENTS) {
-          return cb(new (Errors.DBUpdateError)('Demasiados intentos.'));
-        }
-        setTimeout(function() {
-          User.findOne(
-            {userid: user.userid},
-            function(err, doc) {
-              if (err) {console.error(err); return cb(err, doc);}
-
-              buyUpgrade(user, upgrade, cb, ++count);
-            }
-          );
-        }, 250 * count + Math.random() * 250);
-        return;
-      }
-      cb(doc);
+  User.intent({
+    user: user,
+    update: {
+      $inc: {clicks: 1},
+      $set: {lastClick: Date.now()}
     }
-  );
+  }, cb);
+};
+
+userSchema.methos.buyUpgrade = function buyUpgrade(upgrade, cb) {
+  var User = mongoose.model('User');
+  var user = this.
+  User.intent({
+    user: user,
+    query: function(user) {
+      var findQuery = {updgrades: {id: upgrade.id}};
+      if (!_.filter(user.upgrades, _.matches({id: upgrade.id}))) {
+        delete findQuery.upgrades;
+      }
+      return findQuery;
+    },
+    update: function(user) {
+      if (user.getTotalClicks() < upgrade.getCost(user)) {
+        return new UserStatusError('No tienes suficientes clicks para vender.');
+      }
+      var updateQuery =  {
+        $push: {'upgrades.$.buyed': Date.now()},
+        $set: {lastBuy: Date.now()}
+      };
+      if (!_.filter(user.upgrades, _.matches({id: upgrade.id}))) {
+        delete updateQuery.$push;
+        updateQuery.$addToSet = {
+          upgrades: {id: upgrade.id, buyed: [Date.now()]}
+        };
+      }
+      return updateQuery;
+    }
+  }, cb);
 };
 
 userSchema.statics.resetUser = function(user, cb) {
